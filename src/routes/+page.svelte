f8af4b6d71c01971d4690227b53d7c530e6c591f
LuckyMika <61615177+LuckyMika@users.noreply.github.com>
2026-02-03 18:08:29 +0100

    Added basic landing page skeleton

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	let accumulator = 0;
+	let mnemonic = 'mov';
+	let operand = '100';
+</script>
+
+<div class="h-dvh bg-neutral-800">
+	<h1 class="text-center text-4xl font-bold text-green-400">Megamaschine</h1>
+
+	<div class="my-5 flex items-center justify-evenly">
+		<div class="">
+			<span class="min-w-35 border-2 border-white px-2 py-1 text-center text-white"
+				>{accumulator}</span
+			>
+		</div>
+		<div class="flex">
+			<span class="m-0 min-w-15 border-2 border-white px-2 py-1 text-center text-white"
+				>{mnemonic}</span
+			>
+			<span class="m-0 min-w-15 border-2 border-white px-2 py-1 text-center text-white"
+				>{operand}</span
+			>
+		</div>
+	</div>
+</div>
+
